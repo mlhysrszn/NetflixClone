d914b137a08bca9b3fa4048da5801445500ea3e2
Melih Yaşar SÖZEN <mlhysrszn@gmail.com>
2021-08-04 16:02:51 +0300

    Films fragment added

---
+++ app/src/main/java/com/mlhysrszn/netflixclone/ui/FilmsFragment.kt
@@ -0,0 +1,30 @@
+package com.mlhysrszn.netflixclone.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mlhysrszn.netflixclone.R
+
+class FilmsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FilmsFragment()
+    }
+
+    private lateinit var viewModel: FilmsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_films, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/mlhysrszn/netflixclone/ui/FilmsViewModel.kt
@@ -0,0 +1,7 @@
+package com.mlhysrszn.netflixclone.ui
+
+import androidx.lifecycle.ViewModel
+
+class FilmsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
