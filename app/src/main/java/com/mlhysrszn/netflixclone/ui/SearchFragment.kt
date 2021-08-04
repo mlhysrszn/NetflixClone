9ce0f080a772f7d9f5716283d50c2d0b193ed847
Melih Yaşar SÖZEN <mlhysrszn@gmail.com>
2021-08-04 17:18:00 +0300

    Search fragment added

---
+++ app/src/main/java/com/mlhysrszn/netflixclone/ui/SearchFragment.kt
@@ -0,0 +1,30 @@
+package com.mlhysrszn.netflixclone.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mlhysrszn.netflixclone.R
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private lateinit var viewModel: SearchViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/mlhysrszn/netflixclone/ui/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.mlhysrszn.netflixclone.ui
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
